f032383543252f67287cf9777721de26e1da74ff
SPraveeen <praveenji8498@gmail.com>
2025-07-11 07:00:02 +0530

    difficult level questions

--- Analysis_SQL.sql
+++ Analysis_SQL.sql
@@ -157,3 +157,34 @@ Use a WITH clause to calculate the difference between the highest and lowest ene
 Find tracks where the energy-to-liveness ratio is greater than 1.2.
 Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
 */
+
+-- 1.Find the top 3 most-viewed tracks for each artist using window functions.
+
+with ranking_artist as(
+select artist,track,sum(views) as total_view,
+dense_rank()over(partition by artist order by sum(views) desc) as rank
+from spotify
+group by 1,2
+order by 1,3 desc)
+select * from ranking_artist
+where rank <=3;
+
+-- 2.Write a query to find tracks where the liveness score is above the average.
+select track,artist,liveness from spotify
+where liveness > (select avg(liveness) from spotify);
+
+
+--3.Use a WITH clause to calculate the difference between the highest and 
+--lowest energy values for tracks in each album.
+
+with cte as(
+select album,
+max(energy) as highest_energy,
+min(energy) as lowest_energy
+from spotify
+group by 1
+)
+select album,highest_energy-lowest_energy as energy_diff
+from cte
+order by 2 desc;
+
